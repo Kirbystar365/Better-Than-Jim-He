4b026a1e3931f7dd6fa90f549203ab3404bfc093
Kirbystar365 <kirbysstar365@gmail.com>
2025-02-16 03:51:28 -0800

    Create Globals.cs

---
+++ Scripts/Globals.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+public class Globals : MonoBehaviour
+{
+    public static bool win = false;
+    public static float jimHealth = 100;
+    public static float jonHealth = 100;
+}
